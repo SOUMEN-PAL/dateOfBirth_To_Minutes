2ebd9005c57e26c2d69baff7871245acb50bea2c
Soumen <soumenpal3241@gmail.com>
2023-10-03 16:55:03 +0530

    Updated

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -3,37 +3,60 @@ package com.example.dobcalc
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.DatePicker
+import android.widget.TextView
 import android.widget.Toast
+import java.lang.NullPointerException
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInMinutes : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnDatePicker : Button = findViewById(R.id.button)
+        val btnDatePicker = findViewById<Button>(R.id.button)
         btnDatePicker.setOnClickListener {
             clickDatePicker()
         }
     }
 
     fun clickDatePicker(){
-        val myClender = Calendar.getInstance()
-        val year = myClender.get(Calendar.YEAR)
-        val month = myClender.get(Calendar.MONTH)
-        val day = myClender.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this , DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-//            Toast.makeText(this , "Btn date picker displayed" , Toast.LENGTH_LONG).show()
-        },
+        val selectedDate : TextView = findViewById(R.id.tv4)
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val date = calendar.get(Calendar.DAY_OF_MONTH)
+        val datePicker = DatePickerDialog(this ,
+            DatePickerDialog.OnDateSetListener{ view , year , month , day ->
+                var date:String = "$day.${month+1}.$year"
+                selectedDate.text = date
+                tvInMinutes = findViewById(R.id.tv6)
+                var res = toMinutes(date)
+                tvInMinutes?.text = res
+            },
             year,
             month,
-            day
-        ).show()
+            date
+        )
+        datePicker.show()
 
+    }
 
+    fun toMinutes(date:String):String{
+        val sdf = SimpleDateFormat("dd.MM.yyyy" , Locale.ENGLISH)
+        val theDate = sdf.parse(date)
+        val selectedDateInMinutes = theDate.time/60000
+        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+        val currentDateInMinutes = currentDate.time/60000
+        val differenceMinutes = currentDateInMinutes - selectedDateInMinutes
+        return differenceMinutes.toString()
     }
 
 }
